f64384cdddf0b4b323047551120fd3d21890745a
alefagi <iltu@libero.it>
2021-10-19 01:09:50 +0200

    added 'any' route (not auth) to guest.home

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
-
 Auth::routes(['register' => false]);
 
 Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function() {
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource('posts', 'PostController');
 });
+
+Route::get('{any?}', function() {
+    return view('guest.home');
+})->where('any', '.*');
