5fbf3e9d3c25ea927122c22e5c83b36fa2f7b9bf
alefagi <iltu@libero.it>
2021-10-19 19:55:31 +0200

    added a seeder (data by Faker class)

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->title = $faker->text(100);
+            $post->content = $faker->paragraphs(2, true);
+            $post->image = $faker->imageUrl(250, 250);
+            $post->slug = Str::slug($post->title, '-');
+
+            $post->save();
+        }
+    }
+}
