70168a5548b16ab8c8bb886d170b1b06d9ceb547
alefagi <iltu@libero.it>
2021-10-20 01:57:32 +0200

    added validation message on each input

--- resources/views/partials/admin/posts/form.blade.php
+++ resources/views/partials/admin/posts/form.blade.php
@@ -11,17 +11,32 @@
 
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $post->title) }}">
+    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $post->title) }}">
+    @error('title') 
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+    @enderror
   </div>
 
   <div class="form-group">
     <label for="content">Content</label>
-    <textarea class="form-control" id="content" name="content" rows="5">{{ old('content', $post->content) }}</textarea>
+    <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="5">{{ old('content', $post->content) }}</textarea>
+    @error('content') 
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+    @enderror
   </div>
 
   <div class="form-group">
     <label for="image">Image</label>
-    <input type="text" class="form-control" id="image" name="image" value="{{ old('image', $post->image) }}">
+    <input type="text" class="form-control @error('image') is-invalid @enderror" id="image" name="image" value="{{ old('image', $post->image) }}">
+    @error('image') 
+      <div class="invalid-feedback">
+        {{ $message }}
+      </div>
+    @enderror
   </div>
   
   <button type="submit" class="btn btn-success">Save</button>
